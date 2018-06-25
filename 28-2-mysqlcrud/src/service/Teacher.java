df79f21e1d5308c6f88c9c36390aa29f758b25d5
Administrator <Administrator@smart000>
2018-06-25 17:09:11 +0900

    [송유빈] Teacher 클래스 추가

---
+++ 28-2-mysqlcrud/src/service/Teacher.java
@@ -0,0 +1,20 @@
+package service;
+
+public class Teacher {
+	public int teacherName;
+	public int teacherAge;
+	
+	
+	public int getTeacherName() {
+		return teacherName;
+	}
+	public void setTeacherName(int teacherName) {
+		this.teacherName = teacherName;
+	}
+	public int getTeacherAge() {
+		return teacherAge;
+	}
+	public void setTeacherAge(int teacherAge) {
+		this.teacherAge = teacherAge;
+	}
+}
