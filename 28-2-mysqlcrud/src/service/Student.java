ed2ec0096cba885e29a53ff52b9b8c4c6eb16078
Administrator <Administrator@smart000>
2018-06-25 16:47:43 +0900

    student dto 추가

--- 28-2-mysqlcrud/src/service/Student.java
+++ 28-2-mysqlcrud/src/service/Student.java
@@ -1,5 +1,19 @@
 package service;
 
 public class Student {
-
+	private int teacherNo;
+	private int teacherAddrContent;
+	
+	public int getTeacherNo() {
+		return teacherNo;
+	}
+	public void setTeacherNo(int teacherNo) {
+		this.teacherNo = teacherNo;
+	}
+	public int getTeacherAddrContent() {
+		return teacherAddrContent;
+	}
+	public void setTeacherAddrContent(int teacherAddrContent) {
+		this.teacherAddrContent = teacherAddrContent;
+	}
 }
